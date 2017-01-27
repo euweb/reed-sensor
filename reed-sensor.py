0d35f47fc945f30eaba1b3df5850c1145a709ef4
euweb <eugen_w@yahoo.de>
2017-01-27 01:55:07 +0100

    command line args added

--- reed-sensor.py
+++ reed-sensor.py
@@ -1,31 +1,87 @@
 #!/usr/bin/env python
 
 import time
+from datetime import datetime
+import sys
+import getopt
 import RPi.GPIO as GPIO
-
-# setup for raspberry pi 2 B
-# RPi.GPIO board layout 
-GPIO.setmode(GPIO.BOARD)
 
 # Pin 18 (GPIO 24)
 PIN=18
+RRDB_NAME='counter.rrd'
 
-# Pin PIN as input, pull-up (3.3V)
-GPIO.setup(PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+def main(argv):                          
+    counter = None 
+    logfile = None
 
-# polling in a loop, until interrupted by Ctrl-C
-while 1:
+    try:                                
+        opts, args = getopt.getopt(argv, "hdc:f:", ["help","debug","counter=","logfile="])
+    except getopt.GetoptError:          
+        usage()                         
+        sys.exit(2)                     
+    for opt, arg in opts:                
+        if opt == '-d':
+            global _debug
+            _debug = 1
+        elif opt == '-h':
+            usage();
+            sys.exit(0)
+        elif (opt in ("-c", "--counter")): 
+            try:
+                counter = float(arg)
+            except ValueError:
+                print("counter not a number")
+                sys.exit(2)               
+        elif (opt in ("-f", "--logfile")): 
+            logfile = arg               
 
- try:
-  
-  if GPIO.input(PIN):
-    print('Input was HIGH')
-  else:
-    print('Input was LOW')
-  
-  time.sleep(1)
+    #try to get counter from database
 
- except KeyboardInterrupt:
-        GPIO.cleanup()
-        
-GPIO.cleanup()
+    print counter
+    print logfile
+    try:
+         test = CountProcessor(PIN,counter,RRDB_NAME,logfile)
+         while 1:
+             #global datetime
+             print "alive: " + str(datetime.now()) +" input was: " + str(GPIO.input(PIN)) + " current: " + str(test.counter)
+             time.sleep(10)
+    except KeyboardInterrupt:
+         GPIO.remove_event_detect(PIN)
+         GPIO.cleanup()
+         f.close()
+
+def usage():
+    print """
+Usage:
+    -h                   -   print this message
+    -c <counter value>   -   initial value for the counter (step is always 0.01)
+    -d                   -   debug mode (for future use)
+    -f <logfile name>    -   store the counter events in a log file
+    """
+
+
+class CountProcessor:
+    def __init__(self, pin, init_counter, rrdb, logfile):
+        self.pin = pin
+        self.counter = init_counter
+        self.rrdb = rrdb
+        self.logfile = logfile
+        self.setup_gpio(pin)
+
+    def setup_gpio(self,pin):
+        # setup for raspberry pi 2 B
+        # RPi.GPIO board layout
+        GPIO.setmode(GPIO.BOARD)
+        # Pin PIN as input, pull-up (3.3V)
+        GPIO.setup(PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(PIN, GPIO.FALLING, callback=self.on_impulse, bouncetime=2000)
+        print "setup"
+
+    def on_impulse(self,channel):
+        self.counter += 0.01
+        now = str(datetime.now())
+        print now + 'on impuxlse: ' + str(pin) + ' counter=' + str(counter) + " pinvalue=" + str(GPIO.input(self.pin))
+        #self.logfile.write( str(datetime.now()) + ' ' + str(counter) + ' ' + str(GPIO.input(self.pin)) + '\n' )
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
